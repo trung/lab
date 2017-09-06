a754a32baaf6ec67491aaed33573b4dc7ecbe9b0
trung <trung@mdkt.org>
2017-09-06 12:01:03 -0400

    foo

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_security_group" "allow_all" {
+  name        = "foo"
+  description = "inbound traffic"
+}
+
 data "aws_availability_zones" "all" {
 
 }
