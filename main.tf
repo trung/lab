2c1ceae341b1540274f774d92ea63965d1e23040
trung <trung@mdkt.org>
2017-09-07 09:11:23 -0400

    variable

--- main.tf
+++ main.tf
@@ -2,14 +2,25 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "my_var" {
+  default = "default value"
+}
+
 resource "aws_security_group" "allow_all" {
   vpc_id = "vpc-d7adefae"
-  name        = "foo"
-  description = "inbound traffic"
+  name        = "tfe-testing-sg"
+  description = "Testing TFE"
+  tags {
+    Name = "foo-${uuid()}"
+  }
 }
 
 data "aws_availability_zones" "all" {
 
+}
+
+output "myvar" {
+  value = "${var.my_var}"
 }
 
 output "azs" {
